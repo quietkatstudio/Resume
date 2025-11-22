bec5de790db7f933a7f4b0a248681de4afec79a3
quietkatstudio <quietkat.studio@gmail.com>
2025-11-22 12:25:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -97,9 +97,9 @@ window.addEventLister('scroll', () => {
     }
 )};
 
-//backToTopBtn.addEventListener('click', () => {
-//    window.scrollTo({ top: 0, behavior: 'smooth'});
-//});
+backToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth'});
+});
 
 
 
